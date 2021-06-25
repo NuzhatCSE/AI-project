a9b184bc2071c8c2f000890d443782d9c649f7f5
NuzhatCSE <tasniafahmidanuzhat@gamil.com>
2021-06-25 01:18:56 -0700

    third commit

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import math
 import pyautogui as p
 import time as t
 
+# Read Camera
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 
-#Read Camera
-cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 def nothing(x):
     pass
+
+
 # window name
-cv2.namedWindow("Color Adjustments",cv2.WINDOW_NORMAL)
+cv2.namedWindow("Color Adjustments", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("Color Adjustments", (300, 300))
 cv2.createTrackbar("Thresh", "Color Adjustments", 0, 255, nothing)
 
@@ -24,9 +26,6 @@ cv2.createTrackbar("Lower_V", "Color Adjustments", 0, 255, nothing)
 cv2.createTrackbar("Upper_H", "Color Adjustments", 255, 255, nothing)
 cv2.createTrackbar("Upper_S", "Color Adjustments", 255, 255, nothing)
 cv2.createTrackbar("Upper_V", "Color Adjustments", 255, 255, nothing)
-
-
-
 
 while True:
     _, frame = cap.read()
@@ -46,11 +45,32 @@ while True:
     u_h = cv2.getTrackbarPos("Upper_H", "Color Adjustments")
     u_s = cv2.getTrackbarPos("Upper_S", "Color Adjustments")
     u_v = cv2.getTrackbarPos("Upper_V", "Color Adjustments")
-
     # Step -3
     lower_bound = np.array([l_h, l_s, l_v])
     upper_bound = np.array([u_h, u_s, u_v])
 
+    # Step - 4
+    # Creating Mask
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+    # filter mask with image
+    filtr = cv2.bitwise_and(crop_image, crop_image, mask=mask)
+
+    # Step - 5
+    mask1 = cv2.bitwise_not(mask)
+    m_g = cv2.getTrackbarPos("Thresh", "Color Adjustments")  # getting track bar value
+    ret, thresh = cv2.threshold(mask1, m_g, 255, cv2.THRESH_BINARY)
+    dilata = cv2.dilate(thresh, (3, 3), iterations=6)
 
 
+    # step -6
+    cv2.imshow("Thresh", thresh)
+    # cv2.imshow("mask==",mask)
+    cv2.imshow("filter==", filtr)
+    cv2.imshow("Result", frame)
+
+    key = cv2.waitKey(25) & 0xFF
+    if key == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
 
